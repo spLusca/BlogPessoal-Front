88b4ad51463548a491ab5c82257a732b7c2170e2
Lucas Pinheiro <lucaspinheiros2@hotmail.com>
2023-06-20 08:58:36 -0300

    Adicionando a Page Login

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 import Navbar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer'
+import Login from './paginas/login/login'
 import './App.css'
 import Home from './paginas/home/Home'
-import { Grid } from '@material-ui/core'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div>
+        <Routes>
+           // Antigo Switch
+           <Route path="/" element={<Login />} />
+         <Route path="/login" element={<Login />} /> 
+          <Route path="/home" element={<Home />} />
+         {/* <Route path="/cadastro" element={<CadastroUsuario />} />  */}
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
