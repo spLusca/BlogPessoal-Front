0c7e11004cbce05318170f34db1c50aa766ef0a3
Lucas Pinheiro <lucaspinheiros2@hotmail.com>
2023-06-27 09:32:05 -0300

    Adicionando Login e Cadastro

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,29 +1,30 @@
-import { useState } from 'react'
-import Navbar from './components/estaticos/navbar/Navbar'
-import Footer from './components/estaticos/footer/Footer'
-import Login from './paginas/login/login'
-import './App.css'
-import Home from './paginas/home/Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-
+import React from "react";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import "./App.css";
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/login";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div>
-        <Routes>
-           // Antigo Switch
-           <Route path="/" element={<Login />} />
-         <Route path="/login" element={<Login />} /> 
-          <Route path="/home" element={<Home />} />
-         {/* <Route path="/cadastro" element={<CadastroUsuario />} />  */}
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
-  )
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div style={{ minHeight: "100vh" }}>
+          <Routes>
+            {" "}
+            // Antigo Switch
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<CadastroUsuario />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App
+export default App;
--- blogpessoal/src/paginas/login/login.tsx
+++ blogpessoal/src/paginas/login/login.tsx
@@ -1,40 +1,82 @@
-import React from 'react';
-import { Grid,Typography, TextField, Button } from '@material-ui/core';
-import {Box} from '@mui/material';
-import { Link } from 'react-router-dom';
+/* eslint-disable prefer-const */
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import { Box } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin'
+import useLocalStorage from 'react-use-localstorage';
 import './Login.css';
 
 function Login() {
+
+    let navigate = useNavigate();
+
+    const [token, setToken] = useLocalStorage('token')
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        })
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+        console.log(JSON.stringify(userLogin))
+    }
+
+    useEffect(() => {
+        if (token != '') {
+            navigate('/home')
+        }
+    }, [token])
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+            await login(`usuarios/logar`, userLogin, setToken)
+            alert('Usuário logado com sucesso!');
+        } catch (error) {
+            alert('Dados do usuário inconsistentes. Erro ao logar!');
+        }
+    }
 
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight: 'bold'}}>Entrar</Typography>
-                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                    <form onSubmit={onSubmit}>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{ fontWeight: 'bold' }} className='bem-vinde'>Bem-vinde</Typography>
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Logar
-                                </Button>
-                            </Link>
+                            <Button className='botao-logar' type='submit' variant='contained'>
+                                ENTRAR
+                            </Button>
                         </Box>
                     </form>
+
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
-                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Ainda não tem conta?</Typography>
                         </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                        <Link to='/cadastro' className='link1'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        </Link>
                     </Box>
                 </Box>
             </Grid>
+
             <Grid xs={6} style={{
                 backgroundImage: `url(https://i.imgur.com/d5bMdDJ.jpg)`,
-                backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
+                backgroundRepeat: 'no-repeat', width: '80vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
             }}>
-
             </Grid>
         </Grid>
     );
